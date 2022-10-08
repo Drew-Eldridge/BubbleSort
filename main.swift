27bdd5e88fb43030b56d8b0e16b57cbec527a047
Andrew Eldridge <andrew-eldridge@codermerlin.codermerlin.com>
2022-10-07 19:46:43 -0500

    added word files and testing files

--- main.swift
+++ main.swift
@@ -4,9 +4,9 @@
 // DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
 // ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
 // Add your code below:
-var pass = 0
-var swaps = 0
-var swapstot = 0
+//var pass = 0
+//var swaps = 0
+//var swapstot = 0
 var loop = false
 var words : [String] = []
 func printLine() -> Array<String> {
@@ -20,22 +20,22 @@ func swap(word:inout [String], firstIndex:Int, secondIndex:Int) {
     let x = word[firstIndex]
     word[firstIndex] = word[secondIndex]
     word[secondIndex] = x
-    swapstot += 1
+//    swapstot += 1
 }
 func bubbleSort(loop:inout Bool){
-    print("Pass: \(pass), Swaps: \(swaps)/\(swapstot), Array: \(words)")
+//    print("Pass: \(pass), Swaps: \(swaps)/\(swapstot), Array: \(words)")
     while loop == false {
         loop = true
         for i in 0..<(wordy.count - 1) {
-            if wordy[i].lowercased() > wordy[i+1].lowercased() {
+            if wordy[i] > wordy[i+1] {
                 swap(word:&wordy, firstIndex:i,secondIndex:(i+1))
                 loop = false
-                swaps += 1
+        //        swaps += 1
             }
         }
-        pass += 1
-        print("Pass: \(pass), Swaps: \(swaps)/\(swapstot), Array: \(wordy)")
-        swaps = 0
+      //  pass += 1
+  //      print("Pass: \(pass), Swaps: \(swaps)/\(swapstot), Array: \(wordy)")
+    //    swaps = 0
     }
 }
 bubbleSort(loop:&loop)
